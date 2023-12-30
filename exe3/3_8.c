423f96fc67cbbeebdb0cda1d13e850c1beb2e790
Yeswanth1277 <99649327+Yeswanth1277@users.noreply.github.com>
2023-12-30 13:28:19 +0530

    Add files via upload

---
+++ exe3/3_8.c
@@ -0,0 +1,10 @@
+#include<math.h>
+#include<stdio.h>
+int main()
+{
+printf("Enter number\n");
+int number;
+scanf("%d",&number);
+printf("Absolute value of %d is %d.",number,abs(number));
+return 0;
+}
